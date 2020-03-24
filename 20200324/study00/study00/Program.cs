ed0e25974898abaf21a322f04928502fc6b067da
leesongyi <okpo0501@naver.com>
2020-03-24 21:59:14 +0900

    최종

--- 20200324/study00/study00/Program.cs
+++ 20200324/study00/study00/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;    // 가장 기본 적인 기능 포함 시키기  'System' 
 
-namespace study00
-{
+//namespace study00
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			//Console.WriteLine("Song E hello world!");
-			Console.WriteLine("");
+			Console.WriteLine("1234");
 		}
 	}
-}
+
